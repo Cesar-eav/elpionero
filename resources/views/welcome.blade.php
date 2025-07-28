cb888f0c668bef47e4db3d9c51aaa22a63ac7b3d
Cesar-eav <cesar.eav@hotmail.es>
2025-07-27 21:17:52 -0400

    Revista Julio Nº3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
     </header>
 
     <div class="fixed bottom-0 left-0 w-full py-2 px-4 md:hidden z-50">
-        <a href="https://drive.google.com/file/d/1qTuBM4XDMgUnSHh9mKFtj4OeNmjvQ2pd/view?usp=sharing" download
+        <a href="https://drive.google.com/file/d/1pZVsG8imcYlP76l3Eas_k-31ONYWxCZA/view?usp=sharing" download
             target="_blank"
             class="block w-full text-center bg-red-600 hover:bg-blue-700 text-white font-bold py-2 rounded">
             Descargar Revista
@@ -115,76 +115,87 @@
                     </div>
 
                     <div class="flex bg-gray-200 border border-gray-300 rounded-md p-6 mb-8">
-                      <div>
-                        <h3 class="text-lg font-semibold text-gray-700 mb-2">Revista Nº1</h3>
-                        <div
-                            class="w-32 h-48 bg-gray-400 rounded-md shadow-md flex items-center justify-center text-white font-bold">
-                            <a href="https://drive.google.com/file/d/1b304pV29d66y29Ib36fhY589WE-2fIJn/view?usp=sharing"
-                                target="_blank">
-                                <img src="/storage/Portada_ED1.jpg">
-                            </a>
-                          </div>
-                        </div>
-
-                            <div class="px-2">
-                                <h3 class="text-lg font-semibold text-gray-700 mb-2">Revista Nº2</h3>
-                                <div
-                                    class="w-32 h-48 bg-gray-400 rounded-md shadow-md flex items-center justify-center text-white font-bold">
-                                    <a href="https://drive.google.com/file/d/1qTuBM4XDMgUnSHh9mKFtj4OeNmjvQ2pd/view?usp=sharing"
-                                        target="_blank">
-                                        <img src="/storage/Portada_ED2.jpeg">
-                                    </a>
-                                </div>
+                        <div>
+                            <h3 class="text-lg font-semibold text-gray-700 mb-2">Revista Nº1</h3>
+                            <div
+                                class="w-32 h-48 bg-gray-400 rounded-md shadow-md flex items-center justify-center text-white font-bold">
+                                <a href="https://drive.google.com/file/d/1b304pV29d66y29Ib36fhY589WE-2fIJn/view?usp=sharing"
+                                    target="_blank">
+                                    <img src="/storage/Portada_ED1.jpg">
+                                </a>
                             </div>
-
-
                         </div>
 
-                        <div class="mt-8 text-center lg:text-left hidden md:block">
-                            <a href="https://drive.google.com/file/d/1qTuBM4XDMgUnSHh9mKFtj4OeNmjvQ2pd/view?usp=sharing"
-                                target="_blank"
-                                class="inline-block bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-full shadow-md transition duration-300">Descargar
-                                Edición Actual</a>
-                            <a href="#"
-                                class="inline-block ml-4 text-gray-700 hover:text-gray-900 font-medium">Archivo de
-                                Revistas</a>
+                        <div class="px-2">
+                            <h3 class="text-lg font-semibold text-gray-700 mb-2">Revista Nº2</h3>
+                            <div
+                                class="w-32 h-48 bg-gray-400 rounded-md shadow-md flex items-center justify-center text-white font-bold">
+                                <a href="https://drive.google.com/file/d/1qTuBM4XDMgUnSHh9mKFtj4OeNmjvQ2pd/view?usp=sharing"
+                                    target="_blank">
+                                    <img src="/storage/Portada_ED2.jpeg">
+                                </a>
+                            </div>
                         </div>
+
+                        <div class="px-2">
+                            <h3 class="text-lg font-semibold text-gray-700 mb-2">Revista Nº3</h3>
+                            <div
+                                class="w-32 h-48 bg-gray-400 rounded-md shadow-md flex items-center justify-center text-white font-bold">
+                                <a href="https://drive.google.com/file/d/1pZVsG8imcYlP76l3Eas_k-31ONYWxCZA/view?usp=sharing"
+                                    target="_blank">
+                                    <img src="/storage/Portada_ED3.jpeg">
+                                </a>
+                            </div>
+                        </div>
+
 
                     </div>
 
+                    <div class="mt-8 text-center lg:text-left hidden md:block">
+                        <a href="https://drive.google.com/file/d/1pZVsG8imcYlP76l3Eas_k-31ONYWxCZA/view?usp=sharing"
+                            target="_blank"
+                            class="inline-block bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-full shadow-md transition duration-300">Descargar
+                            Edición Actual</a>
+                        <a href="#"
+                            class="inline-block ml-4 text-gray-700 hover:text-gray-900 font-medium">Archivo de
+                            Revistas</a>
+                    </div>
+
                 </div>
+
             </div>
         </div>
+    </div>
 
-        <script>
-            const slider = document.getElementById('imageSlider');
-            let counter = 0;
-            const slideWidth = slider.children[0].offsetWidth; // Get width of first image
+    <script>
+        const slider = document.getElementById('imageSlider');
+        let counter = 0;
+        const slideWidth = slider.children[0].offsetWidth; // Get width of first image
 
-            function nextSlide() {
-                counter++;
-                if (counter >= slider.children.length) {
-                    counter = 0;
-                }
-                slider.style.transform = `translateX(-${counter * slideWidth}px)`;
+        function nextSlide() {
+            counter++;
+            if (counter >= slider.children.length) {
+                counter = 0;
             }
+            slider.style.transform = `translateX(-${counter * slideWidth}px)`;
+        }
 
-            function prevSlide() {
-                counter--;
-                if (counter < 0) {
-                    counter = slider.children.length - 1;
-                }
-                slider.style.transform = `translateX(-${counter * slideWidth}px)`;
+        function prevSlide() {
+            counter--;
+            if (counter < 0) {
+                counter = slider.children.length - 1;
             }
+            slider.style.transform = `translateX(-${counter * slideWidth}px)`;
+        }
 
-            // Auto slide (optional)
-            setInterval(nextSlide, 5000);
-        </script>
+        // Auto slide (optional)
+        setInterval(nextSlide, 5000);
+    </script>
 
 
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif
+    @if (Route::has('login'))
+        <div class="h-14.5 hidden lg:block"></div>
+    @endif
 </body>
 
 </html>
