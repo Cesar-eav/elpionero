a2dd3abf3f6deece6b33798c09887cc282a4cd32
Cesar-eav <cesar.eav@hotmail.es>
2025-08-03 18:28:58 -0400

    Menu articulos

--- resources/views/pdfs/articulo.blade.php
+++ resources/views/pdfs/articulo.blade.php
@@ -30,9 +30,16 @@
         </header>
         <div>
 
-            <div class="flex bg-black border border-gray-300 text-white rounded-md px-6 mb-8 sticky">
-                Contactanos - Nosotros - Rincon Wanderino - Editoriales
-            </div>
+        <div class="flex w-full flex-wrap bg-black border border-gray-300 text-white rounded-md px-6 py-3 mb-8 sticky top-0 z-50 gap-4 text-lg font-semibold">
+            <a href="{{ url('/previsualizar-revista/1/pdf') }}" class="hover:text-[#fc5648] transition-colors flex items-center gap-1">
+                Inicio
+            </a>
+            <a href="{{ route('contacto.formulario') }}" class="hover:text-[#fc5648] transition-colors">Contáctanos</a>
+            <a href="{{ url('nosotros') }}" class="hover:text-[#fc5648] transition-colors">Nosotros</a>
+            <a href="#" class="hover:text-[#fc5648] transition-colors">Rincón Wanderino</a>
+            <a href="#" class="hover:text-[#fc5648] transition-colors">Editoriales</a>
+        </div>
+
 
             <!-- Layout principal -->
             <div class="flex flex-col md:flex-row gap-6">
