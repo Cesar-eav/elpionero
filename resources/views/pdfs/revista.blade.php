0a601b2acc515a2a135b22b8b6d8b3c2f5d92f43
Cesar-eav <cesar.eav@hotmail.es>
2025-08-03 20:44:31 -0400

    Hover en menú

--- resources/views/pdfs/revista.blade.php
+++ resources/views/pdfs/revista.blade.php
@@ -32,14 +32,14 @@
         </header>
         <div>
 
-        <div class="flex w-full flex-wrap bg-black border border-gray-300 text-white rounded-md px-6 py-3 mb-8 sticky top-0 z-50 gap-4 text-lg font-semibold">
-            <a href="{{ url('/previsualizar-revista/1/pdf') }}" class="hover:text-[#fc5648] transition-colors flex items-center gap-1">
+        <div class="flex w-full flex-wrap bg-black border border-gray-300 text-[#fc5648] rounded-md px-6 py-3 mb-8 sticky top-0 z-50 gap-4 text-lg font-semibold">
+            <a href="{{ url('/previsualizar-revista/1/pdf') }}" class="hover:text-white transition-colors flex items-center gap-1">
                 Inicio
             </a>
-            <a href="{{ route('contacto.formulario') }}" class="hover:text-[#fc5648] transition-colors">Contáctanos</a>
-            <a href="{{ url('nosotros') }}" class="hover:text-[#fc5648] transition-colors">Nosotros</a>
-            <a href="#" class="hover:text-[#fc5648] transition-colors">Rincón Wanderino</a>
-            <a href="#" class="hover:text-[#fc5648] transition-colors">Editoriales</a>
+            <a href="{{ route('contacto.formulario') }}" class="hover:text-white transition-colors">Contáctanos</a>
+            <a href="{{ url('nosotros') }}" class="hover:text-white transition-colors">Nosotros</a>
+            <a href="#" class="hover:text-white transition-colors">Rincón Wanderino</a>
+            <a href="#" class="hover:text-white transition-colors">Editoriales</a>
         </div>
 
             <!-- Layout principal -->
