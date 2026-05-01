92c1a148028f10d583263acd893e0abd9fa1c02e
Cesar-eav <cesar.eav@hotmail.es>
2026-05-01 11:55:36 -0400

    Pindoor

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,7 +6,7 @@
         <a href="{{ url('/') }}" class="hover:text-white transition-colors flex items-center gap-1">
             Inicio
         </a>
-<a href="https://www.pindoor.cl" class="bg-red text-white transition-colors">Pin<span class="text-[#fc5648]">door</span></a>
+<a href="https://www.pindoor.cl" target="_blank" class="bg-red text-white transition-colors">Pin<span class="text-[#fc5648]">door</span></a>
         <!-- Búsqueda escritorio -->
         <form action="{{ route('buscar') }}" method="GET" class="hidden md:block">
             <div class="relative flex">
@@ -28,10 +28,10 @@
 
     <!-- Enlaces de navegación escritorio -->
     <nav class="hidden md:flex gap-4 items-center">
+        <a href="{{ url('revistas-lista') }}" class="block px-4 py-2 hover:bg-gray-800 hover:text-white transition-colors">Revistas</a>
         <a href="{{ url('columnas') }}" class="hover:text-white transition-colors">Columnas</a>
         <a href="{{ url('noticias') }}" class="hover:text-white transition-colors">Notas</a>
         
-        <a href="{{ url('cable-a-tierra') }}" class="hover:text-white transition-colors">Cable a Tierra</a>
         <a href="{{ route('juegos.index') }}" class="hover:text-white transition-colors">Juegos</a>
         <a href="{{ url('entrevistas') }}" class="hover:text-white transition-colors">Entrevistas</a>
         {{-- HASTA AQUI --}}
@@ -47,8 +47,10 @@
 
             <!-- Menú desplegable -->
             <div id="desktop-dropdown-menu" class="hidden absolute right-0 mt-2 w-48 bg-black border border-gray-300 rounded-md shadow-lg py-2 z-50">
+                        <a href="{{ url('cable-a-tierra') }}" class="block px-4 py-2 hover:text-white transition-colors">Cable a Tierra</a>
+
                 <a href="{{ url('editoriales') }}" class="block px-4 py-2 hover:bg-gray-800 hover:text-white transition-colors">Editoriales</a>
-                <a href="{{ url('revistas-lista') }}" class="block px-4 py-2 hover:bg-gray-800 hover:text-white transition-colors">Revistas</a>
+                
                 <a href="{{ url('nosotros') }}" class="block px-4 py-2 hover:bg-gray-800 hover:text-white transition-colors">Nosotros</a>
                         <a href="{{ route('aportes') }}" class="hover:text-white text-green-600 transition-colors block px-4 py-2 border-t border-gray-700 pt-3 mt-2">
             
@@ -101,12 +103,13 @@
                 </div>
             </form>
         </div>
-        <a href="https://www.pindoor.cl" class="bg-red text-white transition-colors">Pin<span class="text-[#fc5648]">door</span></a>        <a href="{{ url('columnas') }}" class="hover:text-white transition-colors block py-1">Columnas</a>
+        <a href="{{ url('revistas-lista') }}" class="hover:text-white transition-colors block py-1">Revistas</a>
+        <a href="{{ url('columnas') }}" class="hover:text-white transition-colors block py-1">Columnas</a>
         <a href="{{ url('editoriales') }}" class="hover:text-white transition-colors block py-1">Editoriales</a>
         <a href="{{ url('noticias') }}" class="hover:text-white transition-colors block py-1">Notas</a>
         <a href="{{ url('entrevistas') }}" class="hover:text-white transition-colors block py-1">Entrevistas</a>
         <a href="{{ url('cable-a-tierra') }}" class="hover:text-white transition-colors block py-1">Cable a Tierra</a>
-        <a href="{{ url('revistas-lista') }}" class="hover:text-white transition-colors block py-1">Revistas</a>
+        
         <a href="{{ route('juegos.index') }}" class="hover:text-white transition-colors block py-1">Juegos</a>
         <a href="{{ url('nosotros') }}" class="hover:text-white transition-colors block py-1">Nosotros</a>
         <a href="{{ route('aportes') }}" class="hover:text-white transition-colors block py-1 border-t border-gray-700 pt-3 mt-2">
